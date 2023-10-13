ea4d8b4cf1da6381501c7a2d04d0948df17ef146
vumg <ab17100222@gmail.com>
2023-10-12 19:50:07 -0600

    test buttons colored pink

--- flowbite.js
+++ flowbite.js
@@ -3982,7 +3982,7 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.initTabs = void 0;
 var Default = {
     defaultTabId: null,
-    activeClasses: 'text-black hover:text-black dark:text-blue-500 dark:hover:text-blue-500 border-black dark:border-blue-500',
+    activeClasses: 'text-pink-500 hover:text-pink-500 border-black',
     inactiveClasses: 'dark:border-transparent text-gray-500 hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300',
     onShow: function () { },
 };
